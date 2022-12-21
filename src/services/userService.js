0e62ed8240ae853051fe5e338cc6cee9c5c9f582
datnguyen5121 <datnguyen5121@gmail.com>
2022-12-21 17:36:58 +0700

    hello

--- src/services/userService.js
+++ src/services/userService.js
@@ -25,7 +25,7 @@ let handleUserLogin = (email, password) => {
                 //user already exist
                 
                 let user = await db.User.findOne({
-                    attributes:  ['email', 'roleId', 'password'],
+                    attributes:  ['email', 'roleId', 'password','firstName','lastName'],
                     // exclude: ['password'] //define columns that you dont want to show
                     where: {email: email},
                     raw: true
