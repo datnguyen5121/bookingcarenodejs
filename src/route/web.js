cf3da62eefa88277e3eb4692fbaf816ed71b9234
datnguyen5121 <datnguyen5121@gmail.com>
2022-12-23 02:06:30 +0700

    hello

--- src/route/web.js
+++ src/route/web.js
@@ -18,7 +18,7 @@ let initWebRoutes = (app) => {
   router.put('/api/edit-user', userController.handleEditUser);
   router.delete('/api/delete-user', userController.handleDeleteNewUser); //rest API
   
-  router.get('/allcode', userController.getAllCode);
+  router.get('/api/allcode', userController.getAllCode);
 
 
 
